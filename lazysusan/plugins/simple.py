65bdd223b8b5ead1d9053ebc61ea379ed2e85837
RemyD <remyd@civx.us>
2013-01-26 19:49:06 -0500

    Added /bonus command, and stubs for holler and boot

--- lazysusan/plugins/simple.py
+++ lazysusan/plugins/simple.py
@@ -8,3 +8,29 @@ class Echo(CommandPlugin):
     def echo(self, message, data):
         """Repeat everything everything after /echo."""
         self.bot.reply(message, data)
+
+
+class Bonus(CommandPlugin):
+    COMMANDS = {'/bonus': 'bonus'}
+
+    def bonus(self, message, data):
+        message = '+1'
+        self.bot.reply(message, data)
+        self.bot.api.bop()
+
+
+#class Holler(CommandPlugin):
+#    COMMANDS = {'/holler': 'holler'}
+#
+#    def holler(self, message, data):
+#        userID = self.bot.getUserID()
+#        message = 'Sgood' + '%s' % userID.name
+#        self.bot.reply(message, data)
+
+#class Boot(CommandPlugin):
+#    COMMANDS = {'/boot': 'boot'}
+#
+#    @admin_or_moderator_required
+#    def boot(self, message, data):
+#        userID = self.bot.getUserID()
+#        self.bot.api.boot(UserID)
